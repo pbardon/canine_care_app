e36bf0f77726dfed506c8eecdd25d3807e1184f1
pbardondev <pbard42@uw.edu>
2018-02-03 16:24:16 -0800

    got logo working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,6 +22,10 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+  config.serve_static_assets = true
+  config.serve_static_files = true
+
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
