0e42991c761d9f58ea3cd8cdd4f8e519f1df4ef5
pbardondev <pbard42@uw.edu>
2018-03-19 21:46:45 -0700

    remove duplicate paperclip configs and set to https

--- vendor/config/application.rb
+++ vendor/config/application.rb
@@ -8,16 +8,5 @@ Bundler.require(*Rails.groups)
 
 module DogSittingApp
   class Application < Rails::Application
-    config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_protocol => 'http',
-    :url =>':s3_domain_url',
-    :path => '/:class/:attachment/:id_partition/:style/:filename',
-    :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET_DEVELOPMENT'], #these values safely stored in application.yml thanks to figaro!
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
   end
 end
