7516c15ba3a42c1b44f399afc803551894211380
pbardondev <pbard42@uw.edu>
2018-04-09 23:57:28 -0700

    added testing for sitter model

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,39 +1,58 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+    # Settings specified here will take precedence over those in config/application.rb.
 
-  # The test environment is used exclusively to run your application's
-  # test suite. You never need to work with it otherwise. Remember that
-  # your test database is "scratch space" for the test suite and is wiped
-  # and recreated between test runs. Don't rely on the data there!
-  config.cache_classes = true
+    # The test environment is used exclusively to run your application's
+    # test suite. You never need to work with it otherwise. Remember that
+    # your test database is "scratch space" for the test suite and is wiped
+    # and recreated between test runs. Don't rely on the data there!
+    config.cache_classes = true
 
-  # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
+    # Do not eager load code on boot. This avoids loading your whole application
+    # just for the purpose of running a single test. If you are using a tool that
+    # preloads Rails for running tests, you may have to set it to true.
+    config.eager_load = false
 
-  # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_assets  = true
-  config.static_cache_control = 'public, max-age=3600'
+    # Configure static asset server for tests with Cache-Control for performance.
+    config.static_cache_control = 'public, max-age=3600'
 
-  # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+    # Show full error reports and disable caching.
+    config.consider_all_requests_local       = true
+    config.action_controller.perform_caching = false
 
-  # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = false
+    # Raise exceptions instead of rendering exception templates.
+    config.action_dispatch.show_exceptions = false
 
-  # Disable request forgery protection in test environment.
-  config.action_controller.allow_forgery_protection = false
+    # Disable request forgery protection in test environment.
+    config.action_controller.allow_forgery_protection = false
 
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+    # Tell Action Mailer not to deliver emails to the real world.
+    # The :test delivery method accumulates sent emails in the
+    # ActionMailer::Base.deliveries array.
+    config.action_mailer.delivery_method = :test
 
-  # Print deprecation notices to the stderr.
-  config.active_support.deprecation = :stderr
+    # Print deprecation notices to the stderr.
+    config.active_support.deprecation = :stderr
 
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+    # Raises error for missing translations
+    # config.action_view.raise_on_missing_translations = true
+
+    config.paperclip_defaults = {
+        :storage => :s3,
+        :s3_protocol => 'https',
+        :url =>':s3_domain_url',
+        :s3_region => 'us-west-2',
+        :path => '/:class/:attachment/:id_partition/:style/:filename',
+        :s3_credentials => {
+            :bucket => ENV['AWS_BUCKET_DEVELOPMENT'],
+            :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+            :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+        }
+    }
+
+     Paperclip.options[:log] = false
+
+    config.after_initialize do
+        ActiveRecord::Base.logger = Rails.logger.clone
+        ActiveRecord::Base.logger.level = Logger::INFO
+    end
 end
