44847ec81ec4c47f916bd7c0ac754352fd2a8c3c
Peter Bardon <pbard42@uw.edu>
2019-09-17 19:59:37 -0700

    add S3 for production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
+  # Set storage for S3
   config.active_storage.service = :amazon
 end
