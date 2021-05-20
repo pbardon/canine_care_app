713952248c1a5d3dff8467b917cc7c26e67ab14d
Peter Bardon <ptrbardon@gmail.com>
2021-05-19 22:47:30 -0600

    remove paperclip gem

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,21 +36,6 @@ Rails.application.configure do
     # Raises error for missing translations
     # config.action_view.raise_on_missing_translations = true
 
-    config.paperclip_defaults = {
-        :storage => :s3,
-        :s3_protocol => 'https',
-        :url =>':s3_domain_url',
-        :s3_region => 'us-west-2',
-        :path => '/:class/:attachment/:id_partition/:style/:filename',
-        :s3_credentials => {
-            :bucket => ENV['AWS_BUCKET_TEST'],
-            :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-            :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-        }
-    }
-
-    Paperclip.options[:log] = false
-
     config.google_cloud.use_trace = false
     config.google_cloud.use_logging = false
     config.google_cloud.use_error_reporting = false
